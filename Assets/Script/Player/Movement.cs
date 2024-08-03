bec3d34b1d85c783b758886d4c9818a7240dd916
auto2557 <auto16348@gmail.com>
2024-08-03 13:16:50 +0700

    add-hit-block

--- Assets/Script/Player/Movement.cs
+++ Assets/Script/Player/Movement.cs
@@ -6,8 +6,18 @@ public class Movement : MonoBehaviour
     public float speed;
     protected Rigidbody2D rb;
     public Animator animator;
-    protected SpriteRenderer spriteRenderer;
 
+    public GameObject hitblockR;
+    public GameObject hitblockL;
+
+    protected SpriteRenderer spriteRenderer;
+    private bool isFacingRight = true; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     virtual public void MovementPlayer()
     {
@@ -17,14 +27,19 @@ public class Movement : MonoBehaviour
             rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
             Runanimate(true);
 
-            // Flip the character based on movement direction
-            if (direction.x > 0)
+            if (direction.x > 0 && !isFacingRight)
             {
-                spriteRenderer.flipX = false; // Facing right
+                spriteRenderer.flipX = false; 
+                hitblockR.SetActive(true);   
+                hitblockL.SetActive(false);  
+                isFacingRight = true;
             }
-            else if (direction.x < 0)
+            else if (direction.x < 0 && isFacingRight)
             {
-                spriteRenderer.flipX = true; // Facing left
+                spriteRenderer.flipX = true; 
+                hitblockR.SetActive(false);  
+                hitblockL.SetActive(true);   
+                isFacingRight = false;
             }
         }
         else
